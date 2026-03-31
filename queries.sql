b777c2ceb85b1b998638e38cd63373511e8352f0
Изотов Марк <104685166+Markinmarch@users.noreply.github.com>
2026-03-31 20:00:59 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
+------------------------------------------------------------------------------
+--TASK 4--
+SELECT COUNT(*) AS customers_count FROM customers;
+--TASK 5--
+-------------------------------------------------------------------------------
 SELECT
 	--получаем имя, фамилию продавца
 	--количество совершённых продаж
@@ -48,4 +53,83 @@ ORDER by
 	EXTRACT(ISODOW FROM s.sale_date),
 	seller
 ;
+
+--TASK 6--
+-----------------------------------------------------------------------------------------------
+SELECT
+	-- оператор CASE с условиями для присвоения группы
+	CASE
+		WHEN age BETWEEN 16 AND 25 THEN '16-25'
+		WHEN age BETWEEN 26 AND 40 THEN '26-40'
+		WHEN age > 40 THEN '40+'
+	END AS age_category,
+	COUNT(*)
+FROM customers
+-- условия распределения возрастной группы
+WHERE
+	age BETWEEN 16 AND 25 OR
+	age BETWEEN 26 AND 40 OR
+	age > 40
+GROUP BY age_category
+ORDER BY age_category
+;
+
+SELECT 
+	-- выделяем год и месяц,
+	-- берём общее количество уникальных покупателей
+	-- суммируем общую выручку
+	TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+	COUNT(DISTINCT s.customer_id) AS total_customers,
+	FLOOR(SUM(p.price * s.quantity)) AS income
+FROM sales s 
+INNER JOIN products p ON p.product_id = s.product_id
+--и групируем всё это дело по месячной выручке
+GROUP BY selling_month
+ORDER BY selling_month
+;
+
+SELECT 
+	--получаем имя покупателя,
+	--дату продажи
+	--и имя продавца
+	c.first_name || ' ' || c.last_name AS customer,
+	sale_date,
+	e.first_name || ' ' || e.last_name AS seller
+FROM sales s
+INNER JOIN customers c ON c.customer_id = s.customer_id
+INNER JOIN employees e ON e.employee_id = s.sales_person_id
+INNER JOIN products p ON p.product_id = s.product_id
+-- с условием, что цена товара была 0 денег, т.к. по акции
+WHERE p.price = 0
+;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
